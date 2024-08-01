6a2594bc4ba59a51bacc6ddb6245f136408ac6fd
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-08-01 05:11:38 -0700

    pluh

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// 
+import express from 'express'
+import path from 'path'
+import { config } from 'dotenv'
+
+config()
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(express.static('./static'))
+app.get('/', (req, res)=>{
+    res.status(200).sendFile(path.resolve('./static/index.html'))
+})
+app.get('/', (req, res)=>{
+    res.status(200).sendFile(path.resolve('./static/about.html'))
+})
+app.listen(port,()=>{
+    console.log(`Port running is ${port}`)
+})
